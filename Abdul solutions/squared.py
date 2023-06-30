ef82e122e022ed8e557309aa0e34f1684c998739
Abdul-Samad Olagunju <abdulolagunju19@gmail.com>
2023-06-29 22:43:06 -0600

    add squared, need to now use two pointer

---
+++ Abdul solutions/squared.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+        Runtime Complexity: O(nlogn) time complexity
+        Algorithm: Square every value then sort it.
+    """
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        squared = []
+        for i in range(len(nums)):
+            squared.append(nums[i]**2)
+        squared.sort()
+        return squared
